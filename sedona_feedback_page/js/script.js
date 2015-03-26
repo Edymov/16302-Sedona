0849facc514eb3c50c08153c2ee91e34ab3b4771
edymov <edymov@live.ru>
2015-03-26 19:49:18 +0300

    Добавил черновик скрипта добавления информации о туристах; доработал скрипт добавления/уменьшения колличества

--- sedona_feedback_page/js/script.js
+++ sedona_feedback_page/js/script.js
@@ -1,29 +1,92 @@
-$("document").ready(function() {
+$("document").ready(function () {
 
-	var btnMinus = $(".minus");
-	var btnPlus = $(".plus");
+		var btnMinus = $(".minus");
+		var btnPlus = $(".plus");
+		var container = $("#clients-container");
 
-	btnPlus.click(function(e){
+		var template = document.querySelector("#template").innerHTML;
+		Mustache.parse(template);
 
-    e.preventDefault();
-    // alert("Работает!!");
-		var data = $(this).siblings("input").val();
-		$(this).siblings("input").val(parseInt(data) +1);
+		btnPlus.click(function (e) {
 
-  });
+				e.preventDefault();
+				var input = $(this).siblings("input");
+				var data = $(this).siblings("input").val();
+				var dataMin = $(this).siblings("input").attr("data-min");
+				var current = parseInt(data, 10);
+				if (!isNaN(current)) {
+						$(this).siblings("input").val(current + 1);
+				} else {
+						$(this).siblings("input").val(dataMin);
+				}
+				input.on('change', onChange());
+		});
 
-  btnMinus.click(function(e){
+		btnMinus.click(function (e) {
 
-    e.preventDefault();
-    // alert("Работает!!");
-    var data = $(this).siblings("input").val();
-    var dataMin = 0;
-		$(this).siblings("input").val(parseInt(data) -1);
-		if (data < 0){
-			data = dataMin
+				e.preventDefault();
+				var input = $(this).siblings("input");
+				var data = $(this).siblings("input").val();
+				var dataMin = $(this).siblings("input").attr("data-min");
+				var current = parseInt(data, 10);
+				if (!isNaN(current) && current > dataMin) {
+						$(this).siblings("input").val(current - 1);
+				} else {
+						$(this).siblings("input").val(dataMin);
+				}
+
+				input.on('change', onChange());
+
+		});
+
+
+
+		function onChange(e) {
+
+				var input = e.currentTarget;
+
+				var newVal = input.val();
+				var type;
+				if (input.attr("name") == "people-count") {
+						type = "adult";
+				} else if (input.attr("name") == "children-count") {
+						type = "children";
+				}
+
+				renderTemplate(type, curVal - newVal,  newVal);
+
+				var curVal = input.val();
 		}
 
+		function renderTemplate(type, count, number) {
 
-  });
+				alert("sadasdas");
+
+				if (count > 0) {
+					for (var i = 1; i < count; i++) {
+						if (type === "adult") {
+								var string = template;
+								var html = Mustache.render(string, {
+
+										age: "Взрослого",
+										num: number,
+										type: type
+
+								});
+
+								string = html
+						}
+						else if (type === "children") {
+							alert("Hello!");
+						}
+				}
+					container.append(string);
+				}
+					else{
+							var rows = container.find('.row[data-type="' + type + '"]');
+							for(var i = 0; i < count*-1; i++ )
+							$(rows[rows.length]).remove();
+					}
+		}
 
 });
